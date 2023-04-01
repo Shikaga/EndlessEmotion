26dec8e4ef1e470da3ff6d574d54efab94297305
Shikaga <shikaga@gmail.com>
2023-04-01 13:57:58 +0100

    Added a sendMessage function

--- src/chatGPTHandler.js
+++ src/chatGPTHandler.js
@@ -8,9 +8,10 @@ class ChatGPTHandler {
     }
 
     async callChatGPTAPIWithPromptsArray(prompts) {
+        let duplicatePrompts = JSON.parse(JSON.stringify(prompts));
         const response = await axios.post('https://api.openai.com/v1/chat/completions', {
             "model": "gpt-3.5-turbo",
-            "messages": prompts,
+            "messages": duplicatePrompts,
             "temperature": 0.7
         }, {
             headers: {
@@ -22,6 +23,13 @@ class ChatGPTHandler {
         let responseContent = response.data.choices[0].message.content;
         global.logger.info(responseContent);
         return responseContent;
+    }
+
+    async sendMessage(message) {
+        this.prompts.push({"role": "user", "content": message});
+        let response = await this.callChatGPTAPIWithPromptsArray(this.prompts);
+        this.prompts.push({"role": "assistant", "content": response});
+        return response;
     }
 }
 
--- test/chatGPT.test.js
+++ test/chatGPT.test.js
@@ -63,7 +63,39 @@ describe('ChatGPT', function () {
         // Verify that the response from your code matches the mock response
         expect(response).to.equal('Response which should be logged');
         expect(infoLogger.calledWith('Response which should be logged')).to.be.true;
+    });
 
+    it('should send wrap and send the first message', async function () {
+        const mockResponse = {
+            data: {
+                choices: [{
+                    message: {
+                        content: 'This is a mock response from the ChatGPT API.'
+                    }
+                }]
+            }
+        };
 
+        axiosStub = sinon.stub(axios, 'post').resolves(mockResponse);
+
+        let chatGPT = new ChatGPTHandler();
+
+        // Call your code that uses the ChatGPT API
+        const response = await chatGPT.sendMessage("Test");
+
+        //message sent should be wrapped
+        expect(axiosStub.calledWith('https://api.openai.com/v1/chat/completions', {
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": "Test"}],
+            "temperature": 0.7
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': sinon.match.any
+            }
+        })).to.be.true;
+
+        // Verify that the response from your code matches the mock response
+        expect(response).to.equal('This is a mock response from the ChatGPT API.');
     });
 });
