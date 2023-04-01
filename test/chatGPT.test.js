18c3568be53bc327953b99d0003d5b4638a7036e
Shikaga <shikaga@gmail.com>
2023-04-01 14:02:51 +0100

    Added second test

--- test/chatGPT.test.js
+++ test/chatGPT.test.js
@@ -98,4 +98,54 @@ describe('ChatGPT', function () {
         // Verify that the response from your code matches the mock response
         expect(response).to.equal('This is a mock response from the ChatGPT API.');
     });
+
+    it('should send the first message and response when a second message is sent', async function () {
+        const mockResponse = {
+            data: {
+                choices: [{
+                    message: {
+                        content: 'This is a mock response from the ChatGPT API.'
+                    }
+                }]
+            }
+        };
+
+        axiosStub = sinon.stub(axios, 'post').resolves(mockResponse);
+
+        let chatGPT = new ChatGPTHandler();
+
+        // Call your code that uses the ChatGPT API
+        await chatGPT.sendMessage("Test");
+        const response = await chatGPT.sendMessage("Test2");
+
+        //message sent should be wrapped
+        expect(axiosStub.calledWith('https://api.openai.com/v1/chat/completions', {
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": "Test"}],
+            "temperature": 0.7
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': sinon.match.any
+            }
+        })).to.be.true;
+
+        expect(axiosStub.calledWith('https://api.openai.com/v1/chat/completions', {
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                {"role": "user", "content": "Test"},
+                {"role": "assistant", "content": "This is a mock response from the ChatGPT API."},
+                {"role": "user", "content": "Test2"}
+            ],
+            "temperature": 0.7
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': sinon.match.any
+            }
+        })).to.be.true;
+
+        // Verify that the response from your code matches the mock response
+        expect(response).to.equal('This is a mock response from the ChatGPT API.');
+    });
 });
