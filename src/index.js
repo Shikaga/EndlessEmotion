e0b9fac1152250b2b9679853d2bbdb137451ce5c
Shikaga <shikaga@gmail.com>
2023-04-02 08:33:59 +0100

    Added Client

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const winston = require('winston');
+const express = require('express');
+const path = require('path');
 
 let uniqueLoggingFileName = 'app.log' + new Date().toISOString().replace(/:/g, '-');
 let logFileDirectory = 'logs';
@@ -14,6 +16,20 @@ const logger = winston.createLogger({
     ]
 });
 
-
 logger.info('Hello world');
 console.log('Hello again world');
+
+const app = express();
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
