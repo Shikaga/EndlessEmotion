7d050548408803e350b797b731242e901bdc4d22
Shikaga <shikaga@gmail.com>
2023-04-02 08:25:36 +0100

    Added persistance

--- src/dj.js
+++ src/dj.js
@@ -1,25 +1,19 @@
 class DJ {
-    constructor(chatGPTHandler, elevenLabsHandler, spotifyHandler) {
+    constructor(chatGPTHandler, elevenLabsHandler, spotifyHandler, peristsFile) {
         this.chatGPTHandler = chatGPTHandler;
         this.elevenLabsHandler = elevenLabsHandler;
         this.spotifyHandler = spotifyHandler;
         this.primer = "primer";
+        this.followUp = "followUp";
         this.currentChatGPTValues = {};
         this.currentValues = {};
+        this.nextValues = {};
+        this.peristsFile = peristsFile;
     }
 
     async begin() {
-        this.currentChatGPTValues = await this.chatGPTHandler.getNextMessage(this.primer);
-        this.audio = await this.elevenLabsHandler.getAudioFromDialog(this.currentChatGPTValues.message);
-            //Store the Eleven Labs Audio when returned
-        this.trackInfo = await this.spotifyHandler.searchTrackInfo(this.currentChatGPTValues.song, this.currentChatGPTValues.artist);
- 
-        this.currentValues = {
-            audioLocation: "audio",
-            timeToPlay: Date.now() + 5000,
-            trackURI: this.trackInfo.uri,
-            trackStart: Date.now() + this.audio.duration * 1000 //Because the audio will start in 5 seconds, this will start 5 seconds before the dialog ends
-        }
+        this.currentValues = await this.getAllValuesFromPrompt(this.primer);
+        await this.peristFile();
         //Wait till all returned
         //Persist"
             //the audioLocation and time to play for 5 seconds from now
@@ -27,6 +21,30 @@ class DJ {
         
         //Repeat the above
         //Set a timer to Repeat again after the current song has finished.
+    }
+
+    async getAllValuesFromPrompt(prompt) {
+        this.currentChatGPTValues = await this.chatGPTHandler.getNextMessage(prompt);
+        this.audio = await this.elevenLabsHandler.getAudioFromDialog(this.currentChatGPTValues.message);
+            //Store the Eleven Labs Audio when returned
+        this.trackInfo = await this.spotifyHandler.searchTrackInfo(this.currentChatGPTValues.song, this.currentChatGPTValues.artist);
+ 
+        let values = {
+            audioLocation: "audio",
+            timeToPlay: Date.now() + 5000,
+            trackURI: this.trackInfo.uri,
+            trackStart: Date.now() + this.audio.duration * 1000 //Because the audio will start in 5 seconds, this will start 5 seconds before the dialog ends
+        }
+        return values;
+    }
+
+    async peristFile() {
+        const fs = require('fs');
+        let data = {
+            current: this.currentValues,
+            next: this.nextValues
+        }
+        await fs.writeFileSync(this.peristsFile, JSON.stringify(data));
     }
 
     getLastChatGPTValues() {
--- test/dj.test.js
+++ test/dj.test.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect;
 const { DJ } = require('../src/dj');
 const sinon = require('sinon');
+const fs = require('fs');
 
 describe('DJ', function() {
     let chatGPTHandlerMock;
@@ -16,13 +17,25 @@ describe('DJ', function() {
             getAudioFromDialog: sinon.stub().resolves({"audio": "audio", "duration": 180})
         };
 
+        let spotifyStub = sinon.stub();
+        spotifyStub.onCall(0).resolves({uri: "trackURI", duration: "trackStart"});
+        spotifyStub.onCall(1).resolves({uri: "trackURI2", duration: "trackStart2"});
+
         spotifyMock = {
-            searchTrackInfo: sinon.stub().resolves({uri: "trackURI", duration: "trackStart"})
+            searchTrackInfo: spotifyStub
         }
+
+        //delete the test.json file if it exists
+        if (fs.existsSync('logs/test.json')) {
+            fs.unlinkSync('logs/test.json');
+        }
+
+
+
     });
 
     it('should send the primer as its first message and save response', async function () {
-        const dj = new DJ(chatGPTHandlerMock, elevenLabsMock, spotifyMock);
+        const dj = new DJ(chatGPTHandlerMock, elevenLabsMock, spotifyMock, "logs/test.json");
         await dj.begin();
 
         //verify that the primer was sent
@@ -35,7 +48,7 @@ describe('DJ', function() {
     });
 
     it('should send the message to elevenLabs', async function () {
-        const dj = new DJ(chatGPTHandlerMock, elevenLabsMock, spotifyMock);
+        const dj = new DJ(chatGPTHandlerMock, elevenLabsMock, spotifyMock, "logs/test.json");
         await dj.begin();
 
         //verify that elevenLabs was called with the correct message
@@ -43,15 +56,15 @@ describe('DJ', function() {
     });
 
     it('should find the track information from spotify', async function () {
-        const dj = new DJ(chatGPTHandlerMock, elevenLabsMock, spotifyMock);
+        const dj = new DJ(chatGPTHandlerMock, elevenLabsMock, spotifyMock,  "logs/test.json");
         await dj.begin();
 
         //verify that spotify was called with the correct message
         expect(spotifyMock.searchTrackInfo.calledWith("Careless Whisper", "George Michael")).to.be.true;
     });
 
-    it('should persist the audioLocation and time to play for 5 seconds from now', async function () {
-        const dj = new DJ(chatGPTHandlerMock, elevenLabsMock, spotifyMock);
+    it('should save the current values', async function () {
+        const dj = new DJ(chatGPTHandlerMock, elevenLabsMock, spotifyMock, "logs/test.json");
         await dj.begin();
 
         //verify that the audioLocation and time to play for 5 seconds from now was persisted
@@ -60,5 +73,68 @@ describe('DJ', function() {
         expect(currentValues.timeToPlay).to.be.closeTo(Date.now() + 5000, 100);
         expect(currentValues.trackURI).to.equal(`trackURI`);
         expect(currentValues.trackStart).to.be.closeTo(Date.now() + 180 * 1000, 100);
+    });
+
+    it('should persist the current values to file when persistFile called', async function () {
+        const dj = new DJ(chatGPTHandlerMock, elevenLabsMock, spotifyMock, "logs/test.json");
+        // await dj.begin();
+
+        dj.currentValues = {
+            audioLocation: "audio",
+            timeToPlay: Date.now() + 5000,
+            trackURI: "trackURI",
+            trackStart: Date.now() + 180 * 1000
+        }
+
+        dj.peristFile();
+
+        //get test.json and verify that it contains the correct values
+        const fs = require('fs');
+        const data = fs.readFileSync("logs/test.json");
+        let json = JSON.parse(data);
+        const currentValue = json.current;
+        expect(currentValue.audioLocation).to.equal(`audio`);
+        expect(currentValue.timeToPlay).to.be.closeTo(Date.now() + 5000, 100);
+        expect(currentValue.trackURI).to.equal(`trackURI`);
+        expect(currentValue.trackStart).to.be.closeTo(Date.now() + 180 * 1000, 100);
+    });
+
+    it('should persist the next values to file when persistFile called', async function () {
+        const dj = new DJ(chatGPTHandlerMock, elevenLabsMock, spotifyMock, "logs/test.json");
+        // await dj.begin();
+
+        dj.nextValues = {
+            audioLocation: "audio2",
+            timeToPlay: Date.now() + 5000,
+            trackURI: "trackURI2",
+            trackStart: Date.now() + 180 * 1000
+        }
+
+        dj.peristFile();
+
+        //get test.json and verify that it contains the correct values
+        const fs = require('fs');
+        const data = fs.readFileSync("logs/test.json");
+        let json = JSON.parse(data);
+        const nextValue = json.next;
+        expect(nextValue.audioLocation).to.equal(`audio2`);
+        expect(nextValue.timeToPlay).to.be.closeTo(Date.now() + 5000, 100);
+        expect(nextValue.trackURI).to.equal(`trackURI2`);
+        expect(nextValue.trackStart).to.be.closeTo(Date.now() + 180 * 1000, 100);
+    });
+
+    it('should persist the current values when begin called', async function () {
+        const dj = new DJ(chatGPTHandlerMock, elevenLabsMock, spotifyMock, "logs/test.json");
+        await dj.begin();
+
+        //get test.json and verify that it contains the correct values
+        const fs = require('fs');
+        const data = fs.readFileSync("logs/test.json");
+        let json = JSON.parse(data);
+        const currentValue = json.current;
+        expect(currentValue.audioLocation).to.equal(`audio`);
+        expect(currentValue.timeToPlay).to.be.closeTo(Date.now() + 5000, 100);
+        expect(currentValue.trackURI).to.equal(`trackURI`);
+        expect(currentValue.trackStart).to.be.closeTo(Date.now() + 180 * 1000, 100);
     });
 })
