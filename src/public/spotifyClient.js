a534e9668955a1c9034ec814d2be788308f4013e
Shikaga <shikaga@gmail.com>
2023-04-04 22:14:38 +0100

    You can now select your radio

--- src/public/spotifyClient.js
+++ src/public/spotifyClient.js
@@ -1,5 +1,41 @@
 export class SpotifyClient {
     constructor() {
+        this.selectedDeviceId = null;
+    }
+
+    async getListOfDevices() {
+        try {
+            const accessToken = await this.getAccessToken();
+            if (!accessToken) {
+                throw new Error('No access token');
+            }
+            const response = await fetch('https://api.spotify.com/v1/me/player/devices', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+            });
+            response.json().then(data => {
+                let deviceList = document.getElementById("deviceList");
+                //create a list of devices
+                for (let i = 0; i < data.devices.length; i++) {
+                    let device = data.devices[i];
+                    let option = document.createElement("option");
+                    option.text = device.name;
+                    option.value = device.id;
+                    deviceList.add(option);
+                }
+
+                this.selectedDeviceId = deviceList.options[deviceList.selectedIndex].value;
+
+                deviceList.onchange = function () {
+                    this.selectedDeviceId = deviceList.options[deviceList.selectedIndex].value;
+                }.bind(this);
+            });
+        } catch (error) {
+            console.error('Error:', error.message);
+        }
     }
 
     async playSong(songUri, positionInMs) {
@@ -7,7 +43,7 @@ export class SpotifyClient {
         console.log("starting at: " + positionInMs)
         try {
             const accessToken = await this.getAccessToken();
-            const deviceId = "b771d1e95aa687e81c63b4039b7d0a258cf25c53"; //await this.getActiveDevice(accessToken);
+            const deviceId = this.selectedDeviceId;
 
             const response = await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`, {
             method: 'PUT',
